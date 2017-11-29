47d099034143ab3a4209ad6bcbfdaae438c3eb98
luolinyuan <luolinyuanLly@163.com>
2017-11-30 02:52:53 +0800

    dist

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>测试发布搜索</h2>
+    <h2>测试发布搜索s</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
